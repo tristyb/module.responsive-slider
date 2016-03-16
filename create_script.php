091d9319c29d7d040fd4c18a1d05252a934e1545
Tristan <tristan@tristanbrook.es>
2016-03-16 09:51:50 +0000

    Fixes to appease based JED gods

--- create_script.php
+++ create_script.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * TRISTAN'S RESPONSIVE SLIDER
+ * @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die;
+
 class mod_tristans_responsive_sliderInstallerScript
 {
 
--- tmpl/default.php
+++ tmpl/default.php
@@ -5,6 +5,8 @@
  * @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
+ defined('_JEXEC') or die;
+
 ?>
 
 <div class="flexslider">
