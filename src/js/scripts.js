b896cb72dffb175cbe9bf818c0553d6881992f9b
Tristan Brookes <tristan.s.brookes@gmail.com>
2021-07-31 14:36:56 +1000

    Implement height update from github

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,58 @@
 import Glide, { Controls, Swipe } from '@glidejs/glide/dist/glide.modular.esm'
+import { siblings } from '@glidejs/glide/src/utils/dom';
+
+// https://github.com/glidejs/glide/issues/236#issuecomment-765090306
+const classes = {
+  glideSlideNextActive: 'glide__slide--next-active',
+};
+
+const selectors = {
+  glideSlideNextActive: '.glide__slide--next-active',
+  glideTrack: '.glide__track',
+};
+
+function ResizeSlider(Glide, Components, Events) {
+  var Component = {
+    mount() {
+      this.changeActiveSlide();
+      this.updateTrackHeight();
+    },
+
+    changeActiveSlide() {
+      let slide = Components.Html.slides[Glide.index];
+      slide.classList.add(classes.glideSlideNextActive);
+
+      siblings(slide).forEach((sibling) => {
+        sibling.classList.remove(classes.glideSlideNextActive);
+      });
+    },
+
+    updateTrackHeight() {
+      console.log('update track height');
+
+      const activeSlide = document.querySelector(
+        selectors.glideSlideNextActive
+      );
+      const activeSlideHeight = activeSlide ? activeSlide.offsetHeight : 0;
+
+      const glideTrack = document.querySelector(selectors.glideTrack);
+      const glideTrackHeight = glideTrack ? glideTrack.offsetHeight : 0;
+
+      console.log(`Active slide: ${activeSlide} activeSlideHeight: ${activeSlideHeight}`)
+
+      if (activeSlideHeight !== glideTrackHeight) {
+        glideTrack.style.height = `${activeSlideHeight}px`;
+      }
+    },
+  };
+
+  Events.on('run', () => {
+    Component.changeActiveSlide();
+    Component.updateTrackHeight();
+  });
+
+  return Component;
+};
 
 // Equivilant to jQuery ready.
 const ready = function (fn) {
@@ -16,8 +70,10 @@ function setupSlider() {
   sliders.forEach(slider => {
     const glide = new Glide(slider, {
       type: 'carousel',
-    }).mount({ Controls, Swipe });
+    }).mount({ Controls, Swipe, ResizeSlider });
   });
 }
 
-ready(setupSlider);
+window.onload = function () {
+  setupSlider();
+};
